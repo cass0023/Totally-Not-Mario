77d74258a782c4754b64ebc4292df22534f6441e
cass0023 <150483589+cass0023@users.noreply.github.com>
2023-11-18 11:15:01 -0500

    Level.cs

---
+++ Totally_Not_Mario/Totally_Not_Mario/Level.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Raylib_cs;
+using System.Numerics;
+
+namespace Totally_Not_Mario
+{
+    internal class Level
+    {
+        static Draw()
+        {
+            static void GroundLevel()
+            {
+                //need player code so i can detect if player is actively playing to implement ground physics and collision
+            }
+            static bool Brick()
+            {
+                //if top x, y of player hits brick, brick breaks 
+            }
+            static void Background()
+            {
+                //need screen dimensions and know how to change player camera with movement
+            }
+            static bool EndOfLevel()
+            {
+                //when player collides the game will successfully end
+            }
+        }
+
+    }
+}

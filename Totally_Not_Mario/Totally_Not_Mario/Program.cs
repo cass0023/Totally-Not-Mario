ac94734bf810d8c72d73da8a6a21f254f2757cae
Ethan K <ekoocher1616@gmail.com>
2023-11-22 13:33:14 -0500

    Code to draw enemy texture

--- Totally_Not_Mario/Totally_Not_Mario/Program.cs
+++ Totally_Not_Mario/Totally_Not_Mario/Program.cs
@@ -1,5 +1,6 @@
 ﻿//using System.Drawing;
 using System.Numerics;
+using System.Reflection.Metadata.Ecma335;
 using Raylib_cs;
 namespace Totally_Not_Mario;
 
@@ -11,33 +12,56 @@ class Program
         const int width = 1280;
         const int height = 720;
 
+        static Texture2D goomba;
         static void Main(string[] args)
         {
             Raylib.InitWindow(width, height, title);
             Raylib.SetTargetFPS(60);
 
+
+
+
+
+
+            EnemySetup();
+
             while (!Raylib.WindowShouldClose())
             {
                 Raylib.BeginDrawing();
                 Raylib.ClearBackground(Color.RAYWHITE);
+
+                Raylib.DrawTexture(goomba, 200, 200, Color.WHITE);
+
+
+
+
+
                 Raylib.EndDrawing();
             }
             Raylib.CloseWindow();
         }
 
-
         public void Setup()
         {
-            //text
+
+        }
+        static void EnemySetup()
+        {
+            goomba = LoadTexture2D("goombas.png");
         }
 
-
+   
         public void Update()
         {
-
+            Raylib.DrawTexture(goomba, 200, 200, Color.WHITE);
         }
 
-
+        static Texture2D LoadTexture2D(string fileName)
+        {
+            Image image = Raylib.LoadImage($"../../../resources/goomba/{fileName}");
+            Texture2D texture = Raylib.LoadTextureFromImage(image);
+            return texture;
+        }
 
 
     }

5047ecbb470fc0a40cd3ba630524c226c4a90ec6
Jonah Cappa <jonahcappaofficial@gmail.com>
2023-11-22 22:19:46 -0500

    Add comments

--- Totally_Not_Mario/Totally_Not_Mario/Character.cs
+++ Totally_Not_Mario/Totally_Not_Mario/Character.cs
@@ -18,24 +18,24 @@ namespace Totally_Not_Mario
         float frameDelayCounter;
         int frameIndex;
         bool marioMoving;
-        Vector2 marioVelocity;
         int marioSpeed;
-        Vector2 marioPosition;
+        int marioJumpSpeed;
 
 
         public Character()
 		{
-
+            //loads the texture 
             mario = Raylib.LoadTexture("../../../../../Assets/mario-sprite-smaller.png");
+            //how many frames in the sprite
             numFrames = 4;
+            //displays only part of the sprite 
             frameWidth = mario.Width / numFrames;
             frameRec = new Rectangle(0, 0, frameWidth, mario.Height);
+            //where mario is on screen
             position = new Vector2(Raylib.GetScreenWidth() / 2, Raylib.GetScreenHeight() / 2);
+            //how fast mario runs
             marioSpeed = 10;
-
-
             
-  
 
             frameDelay = 2.0f;
             frameDelayCounter = 0.0f;
@@ -47,30 +47,29 @@ namespace Totally_Not_Mario
 
         public void Move()
         {
-
+            //if right key is pressed down mario moves right
             if (Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT))
             {
                 position.X += marioSpeed;
                 marioMoving = true;
             }
-
+            //if left key is pressed down mario moves left 
             else if (Raylib.IsKeyDown(KeyboardKey.KEY_LEFT))
             {
                 position.X -= marioSpeed;
                 marioMoving = true;
-                
             }
-
+            //if nothing is pressed mario stays in place
             else
             {
-
-                marioMoving =false;
+                marioMoving = false;
             }
 
             ++frameDelayCounter;
-            
+            //resets after 4 frames (the number in the sprite)
             if (frameDelayCounter > frameDelay)
             {
+                //if mario is moving the program cylces through all 4 pictures in the sprite to simulate running
                 if (marioMoving)
                 {
                     frameDelayCounter = 0.0f;

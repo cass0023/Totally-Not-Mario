2cec204ccdaa6843bc9e56b76c8488ca8245fd22
cass0023 <150483589+cass0023@users.noreply.github.com>
2023-11-24 15:33:49 -0500

    Add ground texture

---
+++ cass-level/cass-level/Program.cs
@@ -0,0 +1,14 @@
+﻿using Raylib_cs;
+namespace cass_level
+{
+    internal class Program
+    {
+        static Texture2D LoadTexture2D(string groundLevelName)
+        {
+            //loads ground texture to draw
+            Image groundLevel = Raylib.LoadImage("../../../resources/{groundLevelName}");
+            Texture2D groundTexture = Raylib.LoadTextureFromImage(groundLevel);
+            return groundTexture;
+        }
+    }
+}

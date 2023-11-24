18e1da487509b08f1d3448a9cc93c2bb502c193a
Jonah Cappa <jonahcappaofficial@gmail.com>
2023-11-24 15:40:23 -0500

    Add jumping and fix gravity

--- Totally_Not_Mario/Totally_Not_Mario/Character.cs
+++ Totally_Not_Mario/Totally_Not_Mario/Character.cs
@@ -8,59 +8,76 @@ namespace Totally_Not_Mario
 
 
     public class Character
-	{
+    {
+        //texture
         Texture2D mario;
-        int numFrames;
-        int frameWidth;
         Rectangle frameRec;
         Vector2 position;
+        int frameWidth;
+        Vector2 size = new Vector2(95, 78);
+
+        //animation
+        int numFrames;
         float frameDelay;
         float frameDelayCounter;
         int frameIndex;
         bool marioMoving;
-        int marioSpeed;
 
+        //moving/collison 
+        int marioSpeed;
+        bool isOnGround { get; set; }
+        bool isPlayerBottom { get; set; }
+        float playerBottom { get; set; }
+
+        //screen dimensions
         int screenWidth = 1280;
         int screenHeight = 720;
-        Vector2 size = new Vector2(95, 78);
 
-        float keyboardForce = 5;
-        Vector2 velocity;
-        
+        //gravity
+        float friction = 0.2f;
         Vector2 gravityVelocity { get; set; }
-        Vector2 Gravity { get; } = new Vector2(0, 10); // positive Y is downward
+        Vector2 Gravity { get; set; } = new Vector2(0, 50); // positive Y is downward
 
         public Character()
-		{
+        {
             //loads the texture 
             mario = Raylib.LoadTexture("../../../../../Assets/mario-sprite-smaller.png");
             //how many frames in the sprite
             numFrames = 4;
             //displays only part of the sprite 
             frameWidth = mario.Width / numFrames;
-            frameRec = new Rectangle(25, 20, 95, 78);
+            frameRec = new Rectangle(20, 20, 95, 78);
             //where mario is on screen
             position = new Vector2(Raylib.GetScreenWidth() / 2, Raylib.GetScreenHeight() / 2);
             //how fast mario runs
             marioSpeed = 10;
-            
 
             frameDelay = 2.0f;
             frameDelayCounter = 0.0f;
             frameIndex = 0;
-
-            
         }
 
+        public void Update()
+        {
+            Move();
+            SimGravity();
+            PlayerCollison();
+        }
 
         public void Move()
         {
+            //checks if mario is on the ground (which is just the screen rightnow)
+            if (playerBottom == screenHeight)
+            {
+                isOnGround = true;
+            }
 
+            //resets the velocity when not on the ground
+            if (!isOnGround)
+            {
+                gravityVelocity = new Vector2(gravityVelocity.X, gravityVelocity.Y);
 
-            float deltaTime = Raylib.GetFrameTime();
-            float force = keyboardForce * deltaTime; 
-
-
+            }
 
             //if right key is pressed down mario moves right at the speed set
             if (Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT))
@@ -73,19 +90,24 @@ namespace Totally_Not_Mario
             {
                 position.X -= marioSpeed;
                 marioMoving = true;
-                
+
             }
-            else if (Raylib.IsKeyPressed(KeyboardKey.KEY_SPACE))
-            {
-                position.Y -= force ;
-            }
-
-
-
-            //if nothing is pressed mario stays in place
+            //if marios not moving the animation stops
             else
             {
                 marioMoving = false;
+            }
+
+            //if spacece is pressed when mario is on the ground he will jump
+            if (Raylib.IsKeyPressed(KeyboardKey.KEY_SPACE) & isOnGround)
+            {
+                isOnGround = false;
+
+                //after pressing space the gravity reverses
+                if (isOnGround == false)
+                {
+                    gravityVelocity = new Vector2(gravityVelocity.X, -20);
+                }
             }
 
             ++frameDelayCounter;
@@ -100,29 +122,11 @@ namespace Totally_Not_Mario
                     frameIndex %= numFrames;
                     frameRec.X = (float)frameWidth * frameIndex;
                 }
-                
-                
             }
-
-
-
-
-
         }
-
-
-        public void Update()
-        { 
-            Move();
-            SimGravity();
-            //PlayerCollison();
-        }
-
-
 
         public void SimGravity()
         {
-
             // How much time has elapsed in the last frame
             float deltaTime = Raylib.GetFrameTime();
             // Calculate that much time's worth of gravitational force
@@ -132,73 +136,30 @@ namespace Totally_Not_Mario
             // Apply velocity to position
             position += gravityVelocity;
 
-
-
-            Vector2 maxPosition = new Vector2 (screenWidth, screenHeight) - size;
+            //keeps mario within the bounds of the window
+            Vector2 maxPosition = new Vector2(screenWidth, screenHeight) - size;
             position = Vector2.Clamp(position, Vector2.Zero, maxPosition);
-
-
         }
-
 
         public void PlayerCollison()
         {
+            //finds the bottom of mario
+            playerBottom = position.Y + frameRec.Height;
+            //checks if mario is greater than the bottom of the screen
+            isPlayerBottom = playerBottom > screenHeight;
 
-            int posX = 100;
-            int posY = 600;
-            int sizeX = 1100;
-            int sizeY = 20;
-
-            Raylib.DrawRectangle(posX, posY, sizeX, sizeY, Color.BEIGE);
-
-            //ground
-            float groundLeft = posX;
-            float groundRight = posX + sizeX;
-            float groundTop = posY;
-            float groundBottom = posY + sizeY;
-
-            //player 
-            float playerLeft = frameRec.X;
-            float playerRight = frameRec.X + frameWidth;
-            float playerTop = position.Y;
-            float playerBottom = position.Y + frameRec.Height;
-
-            bool isPlayerBottom = playerBottom >= groundTop;
-            bool isPlayerTop = playerTop <= groundTop;
-            bool isPlayerLeft = playerLeft >= groundLeft;
-            bool isPlayerRight = playerRight <= groundRight;
-
-            if (isPlayerBottom && isPlayerTop)
+            //applying friction to mario
+            bool applyFriction = isPlayerBottom;
+            if (applyFriction)
             {
-                //gravityVelocity = new Vector2(gravityVelocity.X, gravityVelocity.Y);
+                gravityVelocity *= friction;
             }
-
-
         }
-
-
-
-
-
-
-
-
-
-
 
         public void Render()
         {
-
             Raylib.DrawTextureRec(mario, frameRec, position, Color.WHITE);
-
-
         }
-
-
-
     }
-
-
-    
 }
 
--- Totally_Not_Mario/Totally_Not_Mario/Program.cs
+++ Totally_Not_Mario/Totally_Not_Mario/Program.cs
@@ -7,7 +7,6 @@ namespace Totally_Not_Mario;
 
 class Program
 {
-
     static Character mario = new Character();
 
     internal class Totally_Not_Mario
@@ -15,7 +14,6 @@ class Program
         const string title = "Totally Not Mario";
         const int width = 1280;
         const int height = 720;
-        
 
         static void Main(string[] args)
         {
@@ -29,105 +27,24 @@ class Program
                 Raylib.BeginDrawing();
                 Raylib.ClearBackground(Color.RAYWHITE);
 
-                
                 Update();
-
 
                 Raylib.EndDrawing();
             }
             Raylib.CloseWindow();
         }
 
-
         static void Setup()
         {
 
-            
-  
+
 
         }
-
 
         static void Update()
         {
             mario.Update();
             mario.Render();
-
         }
-
-
-        /*static void DrawGround()
-        {
-
-            int posX = 100;
-            int posY = 600;
-            int sizeX = 1100;
-            int sizeY = 20;
-
-            Raylib.DrawRectangle(posX, posY, sizeX, sizeY, Color.BEIGE);
-
-            float groundLeft = posX;
-            float groundRight = posX + sizeX;
-            float groundTop = posY;
-            float groundBottom = posY + sizeY;
-
-            
-
-
-
-
-        }*/
-
-
-        static void CheckCollison()
-        {
-
-           
-
-
-
-        }
-
-
-
     }
 }
-
-
-/*Raylib.DrawTextureRec(mario, frameRec, position, Color.WHITE);
-
-               
-
-                if (Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT))
-                {
-                    marioVelocity.X = marioSpeed;
-                }
-
-                else if (Raylib.IsKeyDown(KeyboardKey.KEY_LEFT))
-{
-    marioVelocity.X = -marioSpeed;
-}
-
-else
-{
-    marioVelocity.X = 0;
-}
-
-
-
-
-
-++frameDelayCounter;
-if (frameDelayCounter > frameDelay)
-{
-    frameDelayCounter = 0.0f;
-
-    if (marioMoving)
-    {
-        ++frameIndex;
-        frameIndex %= numFrames;
-        frameRec.X = (float)frameWidth * frameIndex;
-    }
-}
-
-*/

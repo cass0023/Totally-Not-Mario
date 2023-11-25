977e67fb0f320589768459b707bd528fdf9ed649
cass0023 <150483589+cass0023@users.noreply.github.com>
2023-11-25 13:47:35 -0500

    Add brick and ground texture

---
+++ Cass-Level/Cass-Level/Level-Texture.cs
@@ -0,0 +1,30 @@
+﻿using Raylib_cs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cass_Level
+{
+    internal class Level_Texture
+    {
+        static Texture2D groundTexture;
+        static Texture2D brickTexture;
+
+        static Texture2D LoadGround2D()
+        {
+            //loads ground texture to draw
+            Image groundLevel = Raylib.LoadImage($"../../../resources/mariogroundlevel.png/");
+            Texture2D groundTexture = Raylib.LoadTextureFromImage(groundLevel);
+            return groundTexture;
+        }
+        static Texture2D LoadBrick2D()
+        {
+            //loads brick texture for platforms
+            Image brickBlock = Raylib.LoadImage($"../../../resources/mariobrick.png/");
+            Texture2D brickTexture = Raylib.LoadTextureFromImage(brickBlock);
+            return brickTexture;
+        }
+    }
+}

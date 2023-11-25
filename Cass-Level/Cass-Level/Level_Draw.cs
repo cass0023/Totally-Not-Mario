bd11ba6ae1bccfe3d8aa9fcf85aac1001e4139d2
cass0023 <150483589+cass0023@users.noreply.github.com>
2023-11-25 13:47:49 -0500

    add class for draw

---
+++ Cass-Level/Cass-Level/Level_Draw.cs
@@ -0,0 +1,12 @@
+﻿using Raylib_cs;
+using System.Reflection.Metadata.Ecma335;
+//using static System.Net.Mime.MediaTypeNames;
+
+namespace Cass_Level
+{
+    internal class Level_Draw
+    {
+        
+    }
+}
+

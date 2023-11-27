493da2d1e5f111c5cbdd3ea489743eba12c42953
cass0023 <150483589+cass0023@users.noreply.github.com>
2023-11-27 18:06:39 -0500

    Add draw and load texture function

---
+++ Totally_Not_Mario/Totally_Not_Mario/Level_Draw.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Totally_Not_Mario
+{
+    internal class Level_Draw
+    {
+    }
+}

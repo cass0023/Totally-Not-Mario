32894b19f3d0371da7476c63188c817dff72f929
cass0023 <150483589+cass0023@users.noreply.github.com>
2023-11-27 19:10:54 -0500

    Add Drawlevel class and load textures

---
+++ Totally_Not_Mario/Totally_Not_Mario/DrawLevel.cs
@@ -0,0 +1,41 @@
+﻿using Raylib_cs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Totally_Not_Mario
+{
+    internal class DrawLevel
+    {
+        static Texture2D groundTexture;
+        static Texture2D brickTexture;
+        static Texture2D LoadGround2D()
+        {
+            //loads ground texture to draw
+            Image groundLevel = Raylib.LoadImage($"../../../../Assets/mariogroundlevel.png/");
+            Texture2D groundTexture = Raylib.LoadTextureFromImage(groundLevel);
+            return groundTexture;
+        }
+        static Texture2D LoadBrick2D()
+        {
+            //loads brick texture for platforms
+            Image brickBlock = Raylib.LoadImage($"../../../../Assets/mariobrick.png/");
+            Texture2D brickTexture = Raylib.LoadTextureFromImage(brickBlock);
+            return brickTexture;
+        }
+
+        public void DrawGroundTexture()
+        {
+            //draws ground
+            Raylib.DrawTexture(groundTexture, 0, 0, Color.WHITE);
+        }
+        public void DrawBrickTexture()
+        {
+            //draws brick
+            Raylib.DrawTexture(brickTexture, 0, 200, Color.WHITE);
+        }
+    }
+}
